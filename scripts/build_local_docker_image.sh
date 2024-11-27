6e04de57e7873c069772e33c6f4865c98a52d3fe
Glenn Hope <glenn.alexander.hope@gmail.com>
2024-11-27 20:34:31 +0800

    Improve caching

--- scripts/build_local_docker_image.sh
+++ scripts/build_local_docker_image.sh
@@ -2,11 +2,13 @@
 
 set -euxo pipefail
 
+cd "$(dirname "$0")"/../misc/act-docker
+
 # To avoid needing sudo to run docker commands, run this and then log out and back in:
 # sudo usermod -aG docker $USER
 
 # To install buildx on Arch:
 # sudo pacman -S docker-buildx
 
-cd "$(dirname "$0")"/../misc/act-docker
-docker build -t decktricks-act .
+# NOTE: this uses the github repo, NOT the local repo.
+docker buildx build --no-cache-filter="gitclone" -t decktricks-act .
