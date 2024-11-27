9f4daa7b84190b95a9b4b29a5a810474fce2d950
Glenn Hope <glenn.alexander.hope@gmail.com>
2024-11-28 02:17:53 +0800

    Update runs

---
+++ scripts/update_latest_branch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [[ "${GITHUB_ACTIONS-}" != "" && "$ACT" != "true" ]]; then
+    git branch -f latest
+    git push origin latest --force
+else
+    echo "Skipping latest push because not in GitHub Actions."
+fi
