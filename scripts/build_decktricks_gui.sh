4257099bc8e16310bda60a19f3667662519b76da
Glenn Hope <glenn.alexander.hope@gmail.com>
2024-11-29 00:21:19 +0800

    more fixes for godot builds

--- scripts/build_decktricks_gui.sh
+++ scripts/build_decktricks_gui.sh
@@ -6,11 +6,11 @@ set -euxo pipefail
 # [] TODO: warn on local uncommitted git changes
 
 BTYPE="release"
-if [[ "$1" == "debug" ]]; then
+if [[ "${1:-}" == "debug" ]]; then
     BTYPE="debug"
 fi
 
-REPOROOT="$(dirname "$0")"/..
+REPOROOT=$(realpath "$(dirname "$0")"/..)
 cd "$REPOROOT"
 
 TMPDIR=$(mktemp -d)
@@ -18,32 +18,41 @@ TMPBUILD="$TMPDIR/build"
 
 mkdir -p "$TMPBUILD"
 
-# Install scripts
+# Install scripts {{{
 cp scripts/decktricks_install.desktop "$TMPBUILD"
 cp scripts/decktricks_install.sh "$TMPBUILD"
+# }}}
 
-# CLI
+# CLI {{{
 if [[ "$BTYPE" == "debug" ]]; then
     cargo build
 else
     cargo build --release
 fi
 cp target/"$BTYPE"/decktricks "$TMPBUILD"
+# }}}
 
-# GUI Rust libs
+# GUI Rust libs {{{
 pushd gui/rust/
 if [[ "$BTYPE" == "debug" ]]; then
     cargo build
 else
     cargo build --release
 fi
-cp target/"$BTYPE"/libdecktricks_godot_gui.so "$TMPBUILD"
 popd
+# }}}
 
-# GUI Binary
+# GUI Binary {{{
 pushd gui/godot/
 rm -rf build/
 mkdir -p build/
+
+cp "$REPOROOT"/gui/rust/target/"$BTYPE"/libdecktricks_godot_gui.so build/
+
+# This helps godot find the gdextension file correctly:
+timeout 10 godot --headless --editor || true
+timeout 10 godot --headless --editor || true
+
 godot --headless "--export-${BTYPE}" "Linux" 2>&1 | tee /tmp/godot_output.txt
 if grep ERROR /tmp/godot_output.txt; then
     echo 'Errors detected during godot build! Will not continue.'
@@ -51,13 +60,16 @@ if grep ERROR /tmp/godot_output.txt; then
 fi
 cp build/* "$TMPBUILD"
 popd
+# }}}
 
 pushd "$TMPBUILD"
 tar czf "$TMPDIR"/decktricks.tar.gz ./*
 popd
 
-# Artifacts - installer is separate for ease of download
+# Artifacts {{{
 rm -rf "$REPOROOT"/build
 mkdir -p "$REPOROOT"/build
 cp "$TMPDIR"/decktricks.tar.gz "$REPOROOT"/build/
+# Installer is separate for ease of download
 cp "$REPOROOT"/scripts/decktricks_install.desktop "$REPOROOT"/build/
+# }}}
