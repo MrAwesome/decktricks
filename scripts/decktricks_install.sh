d67b83c58ee17ce1fd15cf3aa5bb49eb4b958080
Glenn Hope <glenn.alexander.hope@gmail.com>
2024-11-29 22:32:49 +0800

    Installer fixes

--- scripts/decktricks_install.sh
+++ scripts/decktricks_install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+set -euxo pipefail
 
+DTDIR="$HOME/.local/share/decktricks" 
+mkdir -p "$DTDIR"
+cd "$DTDIR"
+curl -L -O --progress-bar --output-dir /tmp --connect-timeout 60 "https://github.com/MrAwesome/decktricks/releases/download/stable/decktricks.tar.gz"
+tar xvf /tmp/decktricks.tar.gz
+
+mkdir -p "$HOME"/Desktop
+ln -sf "$DTDIR"/decktricks.desktop "$HOME"/Desktop/
+
+# TODO: Add to steam here!
