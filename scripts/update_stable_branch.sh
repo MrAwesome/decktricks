26dd6d5dcb6ee61d6b6a240d0145dbd67a6af57e
Glenn Hope <glenn.alexander.hope@gmail.com>
2024-11-29 23:57:13 +0800

    stable push

--- scripts/update_stable_branch.sh
+++ scripts/update_stable_branch.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 if [[ "${GITHUB_ACTIONS:-}" != "" && "${ACT:-}" != "true" ]]; then
-    git branch -f stable latest
+    git branch -f stable
     git push origin stable --force
 else
     echo "Skipping staging push because not in GitHub Actions."
