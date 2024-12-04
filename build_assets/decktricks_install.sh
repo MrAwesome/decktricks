1ca4309ff4162a7641af5c367df50b117fdd097f
Glenn Hope <glenn.alexander.hope@gmail.com>
2024-12-04 17:19:43 +0800

    Fix unbound var in install script

--- build_assets/decktricks_install.sh
+++ build_assets/decktricks_install.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+ERROR=0
+
 curl -L -O --progress-bar --output-dir /tmp --connect-timeout 60 "https://github.com/MrAwesome/decktricks/releases/download/stable/decktricks.tar.xz"
 
 DTDIR="$HOME/.local/share/decktricks" 
@@ -16,19 +18,7 @@ ADDED_TO_STEAM=1
 echo "+ ./decktricks add-decktricks-to-steam"
 ./decktricks add-decktricks-to-steam || {
     ADDED_TO_STEAM=0
-    cat <<EOF
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-Failed to add Decktricks to Steam! This is most likely because
-you aren't logged in to Steam on this system.
-If you are logged in, please report this error here:
-https://github.com/MrAwesome/decktricks/issues
-
-In SteamOS (Steam Deck), you can add Decktricks to Steam easily:
-1) Right-click the "Decktricks" icon on the desktop
-        (use L2/left-trigger on the Deck or controller)
-2) Select "Add to Steam"
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-EOF
+    ERROR=1
 }
 set -x
 
@@ -39,6 +29,7 @@ if [[ "$ADDED_TO_STEAM" == "1" ]]; then
     echo "Shutting down Steam, please wait..."
     steam -shutdown &> /dev/null || true
 
+    set +x
     for ((i=0; i<30; i++)); do
         if ! pgrep steam > /dev/null; then
             break
@@ -46,6 +37,7 @@ if [[ "$ADDED_TO_STEAM" == "1" ]]; then
         echo "($i/30) Waiting for Steam to shut down..."
         sleep 1
     done
+    set -x
 
     DECKTRICKS_FULL_APPID=$(cat /tmp/decktricks_newest_full_steam_appid)
 
@@ -55,6 +47,7 @@ if [[ "$ADDED_TO_STEAM" == "1" ]]; then
     rm -f /tmp/decktricks_only_init
     touch /tmp/decktricks_only_init
 
+    set +x
     for ((i=0; i<30; i++)); do
         if [[ ! -f /tmp/decktricks_only_init ]]; then
             break
@@ -84,12 +77,28 @@ until you see it. Enjoy!
 =====================
 EOF
 
-if [[ "$ERROR" != "1" ]]; then
+if [[ "$ERROR" == "1" ]]; then
+    if [[ "$ADDED_TO_STEAM" != "1" ]]; then
+        cat <<EOF
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+Failed to add Decktricks to Steam! This is most likely because
+you aren't logged in to Steam on this system.
+If you are logged in, please report this error here:
+https://github.com/MrAwesome/decktricks/issues
+
+In SteamOS (Steam Deck), you can add Decktricks to Steam easily:
+1) Right-click the "Decktricks" icon on the desktop
+        (use L2/left-trigger on the Deck or controller)
+2) Select "Add to Steam"
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+EOF
+    fi
+else
     echo
     echo "Will restart to Game Mode in 10 seconds unless you close this window..."
     sleep 10
     # Try to use the desktop file, and run a (possibly old) return to game mode command if it's not present
-    set +x
+    set -x
     if [[ -f "$HOME"/Desktop/Return.desktop ]]; then
         CMD=$(grep '^Exec=' "$HOME"/Desktop/Return.desktop | sed 's/^Exec=//')
         $CMD
