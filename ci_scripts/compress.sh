b2d8c8bd281bbb15ed686642d08041b51bb87c20
Glenn Hope <glenn.alexander.hope@gmail.com>
2024-12-22 01:16:55 -0500

    Fix bin path for compression

--- ci_scripts/compress.sh
+++ ci_scripts/compress.sh
@@ -8,7 +8,9 @@ cd "$REPOBUILD"
 find .
 cd inside_tar/
 XZ_OPT='--x86 --lzma2' tar -cJf decktricks.tar.xz -- *
+cd "$REPOBUILD"
+mv inside_tar/decktricks.tar.xz .
 
 rm -rf inside_tar
 
-echo "[DEBUG] $REPOBUILD: $(echo; find .)"
+echo "[DEBUG] $(pwd; echo; find .)"
