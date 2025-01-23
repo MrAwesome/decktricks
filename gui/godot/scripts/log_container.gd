3d1d5bdd42cb3326e72d81d714ad282e483a7a9c
Glenn Hope <glenn.alexander.hope@gmail.com>
2025-01-22 20:40:20 -0500

    Remove warning

--- gui/godot/scripts/log_container.gd
+++ gui/godot/scripts/log_container.gd
@@ -1,7 +1,7 @@
 extends Logs
 
 # We need to use _process since L2/R2 are analog triggers and send actions multiple times
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if get_parent().visible:
 		if Input.is_action_just_pressed("ui_next_subtab"):
 			select_next_available()
