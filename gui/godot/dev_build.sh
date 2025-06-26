43ea44b3865121c4d66c2f024ad12139b396c45f
Glenn Hope <glenn.alexander.hope@gmail.com>
2025-06-26 16:04:21 -0400

    Add FOSS games

---
+++ gui/godot/dev_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cd "$(dirname "$0")"
+pushd ../rust
+
+cargo build --release
+popd
+
+tmpdir=$(mktemp -d)
+cp ../rust/target/release/libdecktricks_godot_gui.so "$tmpdir"/libdecktricks_godot_gui.so
+# mv instead of copy to get atomic switch without overwriting a running file
+mv "$tmpdir"/libdecktricks_godot_gui.so build/
--- gui/godot/dev_run.sh
+++ gui/godot/dev_run.sh
@@ -3,15 +3,8 @@
 set -euxo pipefail
 
 cd "$(dirname "$0")"
-pushd ../rust
 
-cargo build --release
-popd
-
-tmpdir=$(mktemp -d)
-cp ../rust/target/release/libdecktricks_godot_gui.so "$tmpdir"/libdecktricks_godot_gui.so
-# mv instead of copy to get atomic switch without overwriting a running file
-mv "$tmpdir"/libdecktricks_godot_gui.so build/
+./dev_build.sh
 
 if command -v godot4; then
     godot4
