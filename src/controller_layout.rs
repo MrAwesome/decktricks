bd5949ed4ac7b9661558da63b4e6a7eacb0f4e9f
Glenn Hope <glenn.alexander.hope@gmail.com>
2025-06-29 18:02:26 -0400

    Fixes for controller config launching

--- src/controller_layout.rs
+++ src/controller_layout.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use crate::prelude::*;
 use crate::utils::get_steam_appid;
 
@@ -6,10 +8,12 @@ const DECKTRICKS_CONTROLLER_LAYOUT_ID: &str = "3512322196";
 pub fn open_controller_config(ctx: &impl ExecCtx) {
     let maybe_appid = get_steam_appid();
     if let Some(appid) = maybe_appid {
-        let layout_steam_url = format!("steam://controllerconfig/{appid}/{DECKTRICKS_CONTROLLER_LAYOUT_ID}");
-        ctx.sys_command("xdg-open", [&layout_steam_url]);
-        // NOTE: you may need to sleep 100ms here and then try again, per
+        let layout_steam_url =
+            format!("steam://controllerconfig/{appid}/{DECKTRICKS_CONTROLLER_LAYOUT_ID}");
+        // This is all based on:
         // https://github.com/AAGaming00/Vesktop/blob/9ffa294aee7c42e05d2f6185b3f5d0ea0312c2f5/src/main/utils/steamOS.ts#L51
+        ctx.sys_command("steam", ["-ifrunning", &layout_steam_url]);
+        std::thread::sleep(Duration::from_millis(100));
+        ctx.sys_command("steam", ["-ifrunning", &layout_steam_url]);
     }
 }
-
