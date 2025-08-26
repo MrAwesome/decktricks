296f685f3991b56ce9b22181853b1cc48517f329
Glenn Hope <glenn.alexander.hope@gmail.com>
2025-08-26 07:09:11 +0200

    Update to 4.4.1 stable

--- scripts/build_local_docker_image.sh
+++ scripts/build_local_docker_image.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"/../misc/act-docker
 # To avoid needing sudo to run docker commands, run this and then log out and back in:
 # sudo usermod -aG docker $USER
 
-systemctl status docker || systemctl start docker
+systemctl status docker --no-pager || systemctl start docker
 
 # To install buildx on Arch:
 # sudo pacman -S docker-buildx
